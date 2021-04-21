2e2ac3b2d2244c3be1b12db7fc9710f4a36f3852
OpMalhar <76428183+OpMalhar@users.noreply.github.com>
2021-04-21 14:52:01 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+function adduserfun(){
+    var player1name=document.getElementById("player1_name_input").value;
+    var player2name=document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1",player1name);
+    localStorage.setItem("player2",player2name);
+
+    window.location.replace("game_page.html");
+}
